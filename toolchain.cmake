b4c859fe1d65622ec8fa598956f7d1d41875998e
Tang Ruiwen <wow.2007.t@163.com>
2017-01-25 21:37:30 +0800

    cmake toolchain file

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,19 +1,30 @@
 # this is required
 SET(CMAKE_SYSTEM_NAME Generic)
 
-set(TOOL_CHAIN_ROOT "/Users/tangruiwen/opt/cross/bin")
+set(TOOL_CHAIN_ROOT "E:/Workspace/OSDEV/sysroot/bin")
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER ${TOOL_CHAIN_ROOT}/clang)
+if(WIN32)
+	SET(CMAKE_C_COMPILER ${TOOL_CHAIN_ROOT}/clang.exe)
 
-SET(CMAKE_CXX_COMPILER ${TOOL_CHAIN_ROOT}/clang++)
+	SET(CMAKE_CXX_COMPILER ${TOOL_CHAIN_ROOT}/clang++.exe)
+	set(CMAKE_LINKER ${TOOL_CHAIN_ROOT}/ld.lld.exe CACHE STRING "ld-link.exe")
 
-set(CMAKE_AR ${TOOL_CHAIN_ROOT}/llvm-ar CACHE STRING "llvm-ar")
-set(CMAKE_LINKER ${TOOL_CHAIN_ROOT}/ld.lld CACHE STRING "ld.lld")
-set(CMAKE_NM ${TOOL_CHAIN_ROOT}/llvm-nm CACHE STRING "llvm-nm")
-set(CMAKE_OBJDUMP ${TOOL_CHAIN_ROOT}/llvm-objdump CACHE STRING "llvm-objdump")
-set(CMAKE_RANLIB  ${TOOL_CHAIN_ROOT}/llvm-ranlib CACHE STRING "llvm-ranlib")
+	set(CMAKE_AR ${TOOL_CHAIN_ROOT}/llvm-ar.exe CACHE STRING "llvm-ar")
+	set(CMAKE_NM ${TOOL_CHAIN_ROOT}/llvm-nm.exe CACHE STRING "llvm-nm")
+	set(CMAKE_OBJDUMP ${TOOL_CHAIN_ROOT}/llvm-objdump.exe CACHE STRING "llvm-objdump")
+	set(CMAKE_RANLIB  ${TOOL_CHAIN_ROOT}/llvm-ranlib.exe CACHE STRING "llvm-ranlib")
+else()
+	SET(CMAKE_C_COMPILER ${TOOL_CHAIN_ROOT}/clang)
 
+	SET(CMAKE_CXX_COMPILER ${TOOL_CHAIN_ROOT}/clang++)
+	set(CMAKE_LINKER ${TOOL_CHAIN_ROOT}/ld.lld CACHE STRING "ld-link.exe")
+
+	set(CMAKE_AR ${TOOL_CHAIN_ROOT}/llvm-ar CACHE STRING "llvm-ar")
+	set(CMAKE_NM ${TOOL_CHAIN_ROOT}/llvm-nm CACHE STRING "llvm-nm")
+	set(CMAKE_OBJDUMP ${TOOL_CHAIN_ROOT}/llvm-objdump CACHE STRING "llvm-objdump")
+	set(CMAKE_RANLIB  ${TOOL_CHAIN_ROOT}/llvm-ranlib CACHE STRING "llvm-ranlib")
+endif()
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH  /Users/tangruiwen/opt/cross/lib/5.0.0)

a3ce42f238a577506c2c9a4f54c35f0602051ce8
moonbreeze <wow.2007.t@163.com>
2017-03-05 22:38:57 +0800

    change hd interface

--- kernel/kernel/tasks/harddisktask.cpp
+++ kernel/kernel/tasks/harddisktask.cpp
@@ -6,15 +6,15 @@
 #include <kernel/kernel.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-IDEChannelRegisters Channels[2];
-IDE_device ide_devices[4];
+
 uint8_t ide_buffer[2048];
-uint32_t ide_irq_invoked = 0;
-uint8_t atapi_packet[12] = {0xA8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-HardDiskTask::HardDiskTask(){
-
+HardDiskTask::HardDiskTask()
+:ata_pm(false),ata_ps(false),ata_sm(false),ata_ss(false)
+{
+    memset(_drivers, 0, 4 * sizeof(ide_private_data));
 }
 
 HardDiskTask::~HardDiskTask() {
@@ -23,219 +23,130 @@ HardDiskTask::~HardDiskTask() {
 
 void HardDiskTask::run() {
     printf("Hard Disk Task begin run\n");
-    ide_initialize(0x1F0, 0x3F6, 0x170, 0x376, 0x000);
+    ata_probe();
+    if (ata_pm) {
+        printf("HD info: \n");
+        printf("HD size = %d \n", _drivers[0].size * 512 / 1024 / 1024);
+        printf("mode = %s \n", _drivers[0].model);
+    }
     while(1){}
 }
 
-uint8_t HardDiskTask::ide_read(uint8_t channel, uint8_t reg) {
-    uint8_t result = 0;
-
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, 0x80 | Channels[channel].nIEN);
-    if (reg < 0x08)
-        result = in_byte(Channels[channel].base + reg - 0x00);
-    else if (reg < 0x0C)
-        result = in_byte(Channels[channel].base  + reg - 0x06);
-    else if (reg < 0x0E)
-        result = in_byte(Channels[channel].ctrl  + reg - 0x0A);
-    else if (reg < 0x16)
-        result = in_byte(Channels[channel].bmide + reg - 0x0E);
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, Channels[channel].nIEN);
-    return result;
-}
-
-void HardDiskTask::ide_write(uint8_t channel, uint8_t reg, uint8_t data) {
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, 0x80 | Channels[channel].nIEN);
-    if (reg < 0x08)
-        out_byte(Channels[channel].base  + reg - 0x00, data);
-    else if (reg < 0x0C)
-        out_byte(Channels[channel].base  + reg - 0x06, data);
-    else if (reg < 0x0E)
-        out_byte(Channels[channel].ctrl  + reg - 0x0A, data);
-    else if (reg < 0x16)
-        out_byte(Channels[channel].bmide + reg - 0x0E, data);
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, Channels[channel].nIEN);
-}
-
-void HardDiskTask::ide_read_buffer(uint8_t channel, uint8_t reg, uint32_t buffer, uint32_t quads) {
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, 0x80 | Channels[channel].nIEN);
-    if (reg < 0x08)
-        read_port_to_buffer(Channels[channel].base  + reg - 0x00, (void*)buffer, quads);
-    else if (reg < 0x0C)
-        read_port_to_buffer(Channels[channel].base  + reg - 0x06, (void*)buffer, quads);
-    else if (reg < 0x0E)
-        read_port_to_buffer(Channels[channel].ctrl  + reg - 0x0A, (void*)buffer, quads);
-    else if (reg < 0x16)
-        read_port_to_buffer(Channels[channel].bmide + reg - 0x0E, (void*)buffer, quads);
-    if (reg > 0x07 && reg < 0x0C)
-        ide_write(channel, ATA_REG_CONTROL, Channels[channel].nIEN);
-}
-
-uint8_t HardDiskTask::ide_polling(uint8_t channel, uint32_t advanced_check) {
-    // (I) Delay 400 nanosecond for BSY to be set:
-    // -------------------------------------------------
-    for(int i = 0; i < 4; i++)
-        ide_read(channel, ATA_REG_ALTSTATUS); // Reading the Alternate Status port wastes 100ns; loop four times
-    // (II) Wait for BSY to be cleared:
-    // -------------------------------------------------
-    wait_ide_interrupt();
-    while (ide_read(channel, ATA_REG_STATUS) & ATA_SR_BSY)
-        ; // Wait for BSY to be zero.
-
-    if (advanced_check) {
-        unsigned char state = ide_read(channel, ATA_REG_STATUS); // Read Status Register.
-
-        // (III) Check For Errors:
-        // -------------------------------------------------
-        if (state & ATA_SR_ERR)
-            return 2; // Error.
-
-        // (IV) Check If Device fault:
-        // -------------------------------------------------
-        if (state & ATA_SR_DF)
-            return 1; // Device Fault.
-
-        // (V) Check DRQ:
-        // -------------------------------------------------
-        // BSY = 0; DF = 0; ERR = 0 so we should check for DRQ now.
-        if ((state & ATA_SR_DRQ) == 0)
-            return 3; // DRQ should be set
-
-    }
-
-    return 0; // No Error.
-}
-
-uint8_t HardDiskTask::ide_print_error(uint32_t driver, uint8_t err) {
-    return err;
-}
-
-void HardDiskTask::ide_initialize(uint32_t bar0, uint32_t bar1, uint32_t bar2, uint32_t bar3, uint32_t bar4) {
-    int i,j, k, count = 0;
-
-    // 1- Detect I/O Ports which interface IDE Controller:
-    Channels[ATA_PRIMARY  ].base  = (bar0 & 0xFFFFFFFC) + 0x1F0 * (!bar0);
-    Channels[ATA_PRIMARY  ].ctrl  = (bar1 & 0xFFFFFFFC) + 0x3F6 * (!bar1);
-    Channels[ATA_SECONDARY].base  = (bar2 & 0xFFFFFFFC) + 0x170 * (!bar2);
-    Channels[ATA_SECONDARY].ctrl  = (bar3 & 0xFFFFFFFC) + 0x376 * (!bar3);
-    Channels[ATA_PRIMARY  ].bmide = (bar4 & 0xFFFFFFFC) + 0; // Bus Master IDE
-    Channels[ATA_SECONDARY].bmide = (bar4 & 0xFFFFFFFC) + 8; // Bus Master IDE
-
-    // 2- Disable IRQs:
-    ide_write(ATA_PRIMARY  , ATA_REG_CONTROL, 2);
-    ide_write(ATA_SECONDARY, ATA_REG_CONTROL, 2);
-
-    // 3- Detect ATA-ATAPI Devices:
-    for (i = 0; i < 2; ++i) {
-        for(j = 0; j < 2; j++) {
-            uint8_t err, type = IDE_ATA, status;
-            ide_devices[count].reserved = 0;    // at start, set all driver to 0
-
-            // (I) Select Drive:
-            ide_write(i ,ATA_REG_HDDEVSEL, 0xA0 | (j << 4));
-            // wait for a interrupt
-            // wait_ide_interrupt();
-
-
-            // (II) Send ATA Identify Command:
-            ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
-            // wait for a interrupt
-            // wait_ide_interrupt();
-
-            // (III) Polling:
-            if (ide_read(i, ATA_REG_STATUS) == 0){
-                // If Status = 0, No Device.
-                continue;
-            }
-
-            while(1) {
-                status = ide_read(i, ATA_REG_STATUS);
-                if ((status & ATA_SR_ERR)) {
-                    // If Err, Device is not ATA.
-                    printf("break point3 status = %d\n", status);
-                    err = 1;
-                    break;
-                }
-                if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ)) break; // Everything is right.
-            }
-
-            // (IV) Probe for ATAPI Devices:
-            if (err != 0) {
-                unsigned char cl = ide_read(i, ATA_REG_LBA1);
-                unsigned char ch = ide_read(i, ATA_REG_LBA2);
-
-                if (cl == 0x14 && ch ==0xEB)
-                    type = IDE_ATAPI;
-                else if (cl == 0x69 && ch == 0x96)
-                    type = IDE_ATAPI;
-                else
-                    continue; // Unknown Type (may not be a device).
-
-                ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY_PACKET);
-                wait_ide_interrupt();
-            }
-
-            // (V) Read Identification Space of the Device:
-            ide_read_buffer(i, ATA_REG_DATA, (unsigned int) ide_buffer, 1);
-
-            // (VI) Read Device Parameters:
-            ide_devices[count].reserved     = 1;
-            ide_devices[count].type         = type;
-            ide_devices[count].channel      = i;
-            ide_devices[count].driver        = j;
-            ide_devices[count].signature    = *((unsigned short *)(ide_buffer + ATA_IDENT_DEVICETYPE));
-            ide_devices[count].capabilities = *((unsigned short *)(ide_buffer + ATA_IDENT_CAPABILITIES));
-            ide_devices[count].commandSets  = *((unsigned int *)(ide_buffer + ATA_IDENT_COMMANDSETS));
-
-            // (VII) Get Size:
-            if (ide_devices[count].commandSets & (1 << 26))
-                // Device uses 48-Bit Addressing:
-                ide_devices[count].size   = *((unsigned int *)(ide_buffer + ATA_IDENT_MAX_LBA_EXT));
-            else
-                // Device uses CHS or 28-bit Addressing:
-                ide_devices[count].size   = *((unsigned int *)(ide_buffer + ATA_IDENT_MAX_LBA));
-
-            // (VIII) String indicates model of device (like Western Digital HDD and SONY DVD-RW...):
-            for(k = 0; k < 40; k += 2) {
-                ide_devices[count].model[k] = ide_buffer[ATA_IDENT_MODEL + k + 1];
-                ide_devices[count].model[k + 1] = ide_buffer[ATA_IDENT_MODEL + k];
-            }
-            ide_devices[count].model[40] = 0; // Terminate String.
-
-            count++;
+void HardDiskTask::ide_select_drive(uint8_t bus, uint8_t i) {
+    if(bus == ATA_PRIMARY) {
+        if (i == ATA_MASTER) {
+            out_byte(ATA_PRIMARY_IO + ATA_REG_HDDEVSEL, 0xA0);
+        } else {
+            out_byte(ATA_PRIMARY_IO + ATA_REG_HDDEVSEL, 0xB0);
+        }
+    } else {
+        if (i == ATA_MASTER) {
+            out_byte(ATA_SECONDARY_IO + ATA_REG_HDDEVSEL, 0xA0);
+        }
+        else {
+            out_byte(ATA_SECONDARY_IO + ATA_REG_HDDEVSEL, 0xB0);
         }
     }
-    // 4- Print Summary:
-    for (i = 0; i < 4; i++)
-        if (ide_devices[i].reserved == 1) {
-            printf(" Found %d Drive %dGB - %s\n",
-                   ide_devices[i].type,         /* Type */
-                    ide_devices[i].size / 1024 / 1024 / 2,               /* Size */
-                    ide_devices[i].model);
+}
+
+uint8_t HardDiskTask::ide_identify(uint8_t bus, uint8_t drive) {
+    uint16_t io = 0;
+    ide_select_drive(bus, drive);
+    if(bus == ATA_PRIMARY) io = ATA_PRIMARY_IO;
+    else io = ATA_SECONDARY_IO;
+    /* ATA specs say these values must be zero before sending IDENTIFY */
+    out_byte(io + ATA_REG_SECCOUNT0, 0);
+    out_byte(io + ATA_REG_LBA0, 0);
+    out_byte(io + ATA_REG_LBA1, 0);
+    out_byte(io + ATA_REG_LBA2, 0);
+    /* Now, send IDENTIFY */
+    out_byte(io + ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
+    printf("Sent IDENTIFY\n");
+    /* Now, read status port */
+    uint8_t status = in_byte(io + ATA_REG_STATUS);
+    if(status)
+    {
+        /* Now, poll untill BSY is clear. */
+        while(in_byte(io + ATA_REG_STATUS) & ATA_SR_BSY != 0) ;
+pm_stat_read:
+        status = in_byte(io + ATA_REG_STATUS);
+        if(status & ATA_SR_ERR)
+        {
+            printf("%d | %d has ERR set. Disabled.\n", bus, drive);
+            return 0;
         }
+        while(!(status & ATA_SR_DRQ)) goto pm_stat_read;
+        printf("%s%s is online.\n", bus==ATA_PRIMARY?"Primary":"Secondary", drive==ATA_PRIMARY?" master":" slave");
+        /* Now, actually read the data */
+
+        for(int i = 0; i<256; i++)
+        {
+            *(uint16_t *)(ide_buffer + i*2) = port_read(io + ATA_REG_DATA);
+        }
+    }
+    return 1;
+}
+
+void HardDiskTask::ide_400ns_delay(uint16_t io) {
+    for(int i = 0;i < 4; i++)
+        in_byte(io + ATA_REG_ALTSTATUS);
+}
+
+void HardDiskTask::ide_poll(uint16_t io) {
+    for(int i=0; i< 4; i++)
+        in_byte(io + ATA_REG_ALTSTATUS);
+
+retry:
+    uint8_t status = in_byte(io + ATA_REG_STATUS);
+    //mprint("testing for BSY\n");
+    if(status & ATA_SR_BSY) goto retry;
+    //mprint("BSY cleared\n");
+retry2:
+    status = in_byte(io + ATA_REG_STATUS);
+    if(status & ATA_SR_ERR)
+    {
+        printf("ERR set, device failure!\n");
+        //abort();
+    }
+    //mprint("testing for DRQ\n");
+    if(!(status & ATA_SR_DRQ)) goto retry2;
+    //mprint("DRQ set, ready to PIO!\n");
+    return;
 }
 
 void HardDiskTask::wait_ide_interrupt() {
     send_recv(RECEIVE, INTERRUPT, &this->_msg);
 }
 
-/**
- *
- * @param reg
- * @param buffer
- * @param sectors num of sectors to read
- */
-void HardDiskTask::read_port_to_buffer(uint8_t reg, void *buffer, uint32_t sectors) {
-    uint16_t * p = (uint16_t *) buffer;
+void HardDiskTask::ata_probe() {
+    /* First check the primary bus,
+	 * and inside the master drive.
+	 */
+    int ret = ide_identify(ATA_PRIMARY, ATA_MASTER);
+    if(ret)
+    {
+        ata_pm = true;
+        ide_private_data *priv = &_drivers[0];
+        priv->commandSets = *((unsigned int *)(ide_buffer + ATA_IDENT_COMMANDSETS));
+        priv->drive = (ATA_PRIMARY << 1) | ATA_MASTER;
 
-    for (int i = 0; i < sectors * SECTOR_SIZE / 2; ++i) {
-        p[i] = port_read(reg);
+        if (priv->commandSets & (1 << 26)){
+            // Device uses 48-Bit Addressing:
+            priv->size = *((unsigned int *)(ide_buffer + ATA_IDENT_MAX_LBA_EXT));
+        } else {
+            // Device uses CHS or 28-bit Addressing:
+            priv->size = *((unsigned int *)(ide_buffer + ATA_IDENT_MAX_LBA));
+        }
+        for(int k = 0; k < 40; k += 2) {
+            priv->model[k] = ide_buffer[ATA_IDENT_MODEL + k + 1];
+            priv->model[k + 1] = ide_buffer[ATA_IDENT_MODEL + k];
+        }
+        priv->model[40] = 0; // Terminate String.
+
     }
+    ide_identify(ATA_PRIMARY, ATA_SLAVE);
 }
+
 
 #ifdef __cplusplus
 extern "C" {
--- kernel/kernel/tasks/harddisktask.h
+++ kernel/kernel/tasks/harddisktask.h
@@ -14,27 +14,24 @@
 #define SECTOR_SIZE		512
 #define SECTOR_BITS		(SECTOR_SIZE * 8)
 
-/* STATUS */
-#define ATA_SR_BSY     0x80    // Busy
-#define ATA_SR_DRDY    0x40    // Drive ready
-#define ATA_SR_DF      0x20    // Drive write fault
-#define ATA_SR_DSC     0x10    // Drive seek complete
-#define ATA_SR_DRQ     0x08    // Data request ready
-#define ATA_SR_CORR    0x04    // Corrected data
-#define ATA_SR_IDX     0x02    // Inlex
-#define ATA_SR_ERR     0x01    // Error
+#define ATA_SR_BSY     0x80
+#define ATA_SR_DRDY    0x40
+#define ATA_SR_DF      0x20
+#define ATA_SR_DSC     0x10
+#define ATA_SR_DRQ     0x08
+#define ATA_SR_CORR    0x04
+#define ATA_SR_IDX     0x02
+#define ATA_SR_ERR     0x01
 
-/* ERROR */
-#define ATA_ER_BBK      0x80    // Bad sector
-#define ATA_ER_UNC      0x40    // Uncorrectable data
-#define ATA_ER_MC       0x20    // No media
-#define ATA_ER_IDNF     0x10    // ID mark not found
-#define ATA_ER_MCR      0x08    // No media
-#define ATA_ER_ABRT     0x04    // Command aborted
-#define ATA_ER_TK0NF    0x02    // Track 0 not found
-#define ATA_ER_AMNF     0x01    // No address mark
+#define ATA_ER_BBK      0x80
+#define ATA_ER_UNC      0x40
+#define ATA_ER_MC       0x20
+#define ATA_ER_IDNF     0x10
+#define ATA_ER_MCR      0x08
+#define ATA_ER_ABRT     0x04
+#define ATA_ER_TK0NF    0x02
+#define ATA_ER_AMNF     0x01
 
-/* COMMAND */
 #define ATA_CMD_READ_PIO          0x20
 #define ATA_CMD_READ_PIO_EXT      0x24
 #define ATA_CMD_READ_DMA          0xC8
@@ -49,14 +46,8 @@
 #define ATA_CMD_IDENTIFY_PACKET   0xA1
 #define ATA_CMD_IDENTIFY          0xEC
 
-/* The commands below are for ATAPI devices */
 #define      ATAPI_CMD_READ       0xA8
 #define      ATAPI_CMD_EJECT      0x1B
-
-/*
- * ATA_CMD_IDENTIFY_PACKET and ATA_CMD_IDENTIFY return a buffer of 512 bytes called the identification space;
- * the following definitions are used to read information from the identification space.
- * */
 
 #define ATA_IDENT_DEVICETYPE   0
 #define ATA_IDENT_CYLINDERS    2
@@ -70,19 +61,12 @@
 #define ATA_IDENT_COMMANDSETS  164
 #define ATA_IDENT_MAX_LBA_EXT  200
 
-/* interface type and master or slave */
 #define IDE_ATA        0x00
 #define IDE_ATAPI      0x01
 
 #define ATA_MASTER     0x00
 #define ATA_SLAVE      0x01
 
-/*
- * Task File is a range of 8 ports which are offsets from BAR0 (primary channel) and/or BAR2 (secondary channel). To exemplify:
- * BAR0 + 0 is first port.
- * BAR0 + 1 is second port.
- * BAR0 + 2 is the third
- * */
 #define ATA_REG_DATA       0x00
 #define ATA_REG_ERROR      0x01
 #define ATA_REG_FEATURES   0x01
@@ -101,62 +85,41 @@
 #define ATA_REG_ALTSTATUS  0x0C
 #define ATA_REG_DEVADDRESS 0x0D
 
-/*
- * The ALTSTATUS/CONTROL port returns the alternate status when read and controls a channel when written to.
- * For the primary channel, ALTSTATUS/CONTROL port is BAR1 + 2.
- * For the secondary channel, ALTSTATUS/CONTROL port is BAR3 + 2.
- * We can now say that each channel has 13 registers. For the primary channel, we use these values:
- * Data Register: BAR0 + 0; // Read-Write
- * Error Register: BAR0 + 1; // Read Only
- * Features Register: BAR0 + 1; // Write Only
- * SECCOUNT0: BAR0 + 2; // Read-Write
- * LBA0: BAR0 + 3; // Read-Write
- * LBA1: BAR0 + 4; // Read-Write
- * LBA2: BAR0 + 5; // Read-Write
- * HDDEVSEL: BAR0 + 6; // Read-Write, used to select a drive in the channel.
- * Command Register: BAR0 + 7; // Write Only.
- * Status Register: BAR0 + 7; // Read Only.
- * Alternate Status Register: BAR1 + 2; // Read Only.
- * Control Register: BAR1 + 2; // Write Only.
- * DEVADDRESS: BAR1 + 3; // I don't know what is the benefit from this register.
- * */
-
 // Channels:
 #define      ATA_PRIMARY      0x00
 #define      ATA_SECONDARY    0x01
 
 // Directions:
 #define      ATA_READ      0x00
-#define      ATA_WRITE     0x01
+#define      ATA_WRITE     0x013
 
-#define     BAR0    0x1F0
-#define     BAR1    0x3F6
-#define     BAR2    0x170
-#define     BAR3    0X376
+#define ATA_PRIMARY_IO 0x1F0
+#define ATA_SECONDARY_IO 0x170
 
-// represent ide channel
-struct _ide_channel_registers{
-    uint16_t base; // I/O port base
-    uint16_t ctrl; // Control port Base
-    uint16_t bmide; // Bus Master IDE
-    uint8_t nIEN; // No Interrupt
-};
+#define ATA_PRIMARY_DCR_AS 0x3F6
+#define ATA_SECONDARY_DCR_AS 0x376
 
-typedef struct _ide_channel_registers IDEChannelRegisters;
-// each one represent a ide devices
-struct _ide_devices{
-    uint8_t reserved;   //0 is Empty, 1 The Driver exist
-    uint8_t channel;    //0 primary channel, 1 secondary channel
-    uint8_t driver;     //0 Master driver, 1 Slaver driver
-    uint16_t type;      //0 ATA , 1 ATAPI
-    uint16_t signature; //Driver Signature
-    uint16_t capabilities; // Features
-    uint32_t commandSets;   // Command Sets Support
-    uint32_t size;      // size in sectors
-    uint8_t model[4];      // Model in string
-};
+#define ATA_PRIMARY_IRQ 14
+#define ATA_SECONDARY_IRQ 15
 
-typedef struct _ide_devices IDE_device;
+#define ATA_IDENT_DEVICETYPE   0
+#define ATA_IDENT_CYLINDERS    2
+#define ATA_IDENT_HEADS        6
+#define ATA_IDENT_SECTORS      12
+#define ATA_IDENT_SERIAL       20
+#define ATA_IDENT_MODEL        54
+#define ATA_IDENT_CAPABILITIES 98
+#define ATA_IDENT_FIELDVALID   106
+#define ATA_IDENT_MAX_LBA      120
+#define ATA_IDENT_COMMANDSETS  164
+#define ATA_IDENT_MAX_LBA_EXT  200
+
+typedef struct {
+    uint8_t drive;
+    uint32_t size;
+    uint32_t commandSets;
+    uint8_t model[40];
+} ide_private_data;
 
 
 
@@ -172,16 +135,19 @@ public:
     virtual void run() override ;
 
 private:
-    uint8_t ide_read(uint8_t channel, uint8_t reg);
-    void ide_write(uint8_t channel, uint8_t reg, uint8_t data);
-    void ide_read_buffer(uint8_t channel, uint8_t reg, uint32_t buffer, uint32_t quads);
-    uint8_t ide_polling(uint8_t channel, uint32_t advanced_check);
-    uint8_t ide_print_error(uint32_t driver, uint8_t err);
-    void ide_initialize(uint32_t bar0, uint32_t bar1, uint32_t bar2, uint32_t bar3, uint32_t bar4);
+    void ide_select_drive(uint8_t bus, uint8_t i);
+    uint8_t ide_identify(uint8_t bus, uint8_t drive);
+    void ide_400ns_delay(uint16_t io);
+    void ide_poll(uint16_t io);
     void wait_ide_interrupt();
-    void read_port_to_buffer(uint8_t reg, void* buffer, uint32_t size);
+    void ata_probe();
 private:
+    bool ata_pm; /* Primary master exists? */
+    bool ata_ps; /* Primary Slave exists? */
+    bool ata_sm; /* Secondary master exists? */
+    bool ata_ss; /* Secondary slave exists? */
     MESSAGE _msg;
+    ide_private_data _drivers[4];
 };
 
 #endif
